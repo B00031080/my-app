25dc99ce81a9cd0d8e88eaa7f61c0a39a1357cd3
B00031080 <33052027+B00031080@users.noreply.github.com>
2021-03-01 10:13:02 +0100

    First Commit
    This is the initial state of this repo.

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+</script>
+
+<style lang="postcss">
+	h2{
+		color:white;
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+<h2 class="bg-blue-300">I can easily develop locally and then push this online.</h2>
+
+<figure>
+	<img alt="Success Kid" src="{successkid}">
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
